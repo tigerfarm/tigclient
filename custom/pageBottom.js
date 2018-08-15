55b646aeaaad42b49cfe5914258febd1d4e407fb
tigerfarm <tigerfarm@gmail.com>
2018-08-15 13:34:17 -0700

    updates

---
+++ custom/pageBottom.js
@@ -0,0 +1,14 @@
+document.write('<div id="bottomBar">');
+// document.write('<hr>');
+// Centered:
+// Logo, just the logo.
+document.write('<img src="custom/logo.jpg" alt="Twilio"/>');
+document.write('<div id="bottomContent">');
+document.write('<div style="padding-bottom: 10px;"><a href="https://github.com/tigerfarm/OwlClient" style="color: rgba(255,255,255,.65);">GitHub project: OwlClient</a></div>');
+document.write('<a href="https://twilio.learnupon.com/store" style="color: rgba(255,255,255,.65);">Training</a>');
+document.write(' | <a href="https://www.twilio.com/support-plans" style="color: rgba(255,255,255,.65);">Support</a>');
+document.write(' | <a href="https://www.twilio.com/company" style="color: rgba(255,255,255,.65);">About Twilio</a>');
+document.write('</div>');
+
+document.write('<div style="padding-top: 10px;">2018 Twilio</div>');
+document.write('</div>');
---
+++ custom/pageTop.js
@@ -0,0 +1,6 @@
+document.write('<div id="topBar">');
+document.write('<table><tr>');
+document.write('<td><a href="/index.html"><img src="custom/companyLogo.jpg" alt="Twilio" style="width:160px; padding-right: 30px;"/></a></td>');
+document.write('<td><h1>Owl Voice Client</h1></td>');
+document.write('</tr></table>');
+document.write('</div>');
