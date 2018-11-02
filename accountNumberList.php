121d2f40c9871e6e07d098b07e7f558165d47e4e
tigerfarm <tigerfarm@gmail.com>
2018-11-02 14:38:48 -0700

    Update

--- accountNumberList.php
+++ accountNumberList.php
@@ -3,12 +3,9 @@ require __DIR__ . '/twilio-php-master/Twilio/autoload.php';
 use Twilio\Rest\Client;
 $client = new Client(getenv('ACCOUNT_SID'), getenv('AUTH_TOKEN'));
 $result = $client->messages->read();
-$i = 0;
-// date_default_timezone_set("UTC");
 $sNumbers = "";
 $separator = ":";
 foreach ($client->incomingPhoneNumbers->read() as $number) {
-    // echo "\xA" . $number->dateCreated->format('Y-m-d H:i') . " : " . $number->phoneNumber;
     $sNumbers = $sNumbers . $number->phoneNumber . $separator;
 }
 if ($sNumbers == "") {
