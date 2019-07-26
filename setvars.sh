469b82ad02c6ca137f13e3dda022bdb714967db5
tigerfarm <tigerfarm@gmail.com>
2019-07-26 10:40:16 -0700

    updates

--- setvars.sh
+++
@@ -1,9 +0,0 @@
-echo "+++ Set variables."
-# ------------------------------------------------------------------------------
-TOKEN_HOST=your_Twilio_Functions_domain
-CLIENT_ID=default_client_id
-export TOKEN_HOST
-export CLIENT_ID
-echo "+ Variables set."
-php echoVars.php
-# ------------------------------------------------------------------------------
